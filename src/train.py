62fc82ef073c810023ba59c5731c2327bcfefd58
Ben Luks <luksben@gmail.com>
2026-08-08 15:53:29 +0100

    use nargs

--- src/train.py
+++ src/train.py
@@ -19,10 +19,10 @@ def parse_args():
     # Data / representation
     parser.add_argument("--root", required=True)
     parser.add_argument("--train-split", default="train-clean-100")
-    parser.add_argument("--val-split", default="dev-clean")
+    parser.add_argument("--val-split", default=None)
     parser.add_argument("--layer", type=int, default=5)
     parser.add_argument("--frame-batch-size", type=int, default=8192)
-    parser.add_argument("--hidden-dim", type=list, default=[])
+    parser.add_argument("--hidden-dim", type=int, nargs="+", default=[])
 
     # Optimization
     parser.add_argument("--lr", type=float, default=1e-3)
